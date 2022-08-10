4db237ba75cba4d7a9d13ad3cd869f6b669e4386
unknown <harisnowpop@gmail.com>
2022-08-10 23:54:09 +0700

    push

--- script.js
+++ script.js
@@ -1,68 +1,41 @@
-// 1. Get data form ✅
-// 2. Validation value ✅
-// 3. Display Mail Software ✅
-// 4. Object
 
 function submitForm() {
-    // Get data form
-    let name = document.getElementById('input-name').value
-    let email = document.getElementById('input-email').value
-    let phone = document.getElementById('input-phone').value
-    let subject = document.getElementById('choose-subject').value
-    let message = document.getElementById('input-message').value
+  // DOM = Document Object Model
+  let name = document.getElementById("input-name").value
+  let email = document.getElementById("input-email").value
+  let phone = document.getElementById("input-phone").value
+  let subject = document.getElementById("choose-subject").value
+  let message = document.getElementById("input-message").value
 
-    console.log(name);
-    
-    console.log(email);
-    
-    console.log(phone);
-    
-    console.log(subject);
-    
-    console.log(message);
-    
+  document.getElementById("input-name").value = ""
+  document.getElementById("input-email").value = ""
+  document.getElementById("input-phone").value = ""
+  document.getElementById("choose-subject").value = ""
+  document.getElementById("input-message").value = ""
 
-    // Validation value
-    if(name == ''){
-        console.log('Nama harus diisi ...');
-        return
-    } 
-    if(email == ''){
-        console.log('Email harus diisi ...');
-        return
-    } 
-    if(phone == ''){
-        console.log('Nomor HP harus diisi ...');
-        return
-    } 
-    if(subject == ''){
-        console.log('Subject harus diisi ...');
-        return
-    } 
-    if(message == ''){
-        console.log('Pesan harus diisi ...');
-        return
-    } 
+  // VALIDATION
+  if (name, email, phone, subject, message == "") {
+      return alert("All form input must be filled to continue")
+  }
 
-    console.log('Semua form telah diisi ✅');
+  let emailReceiver = "harihandika2441998@gmail.com"
 
-    // Display Mail Software
-    console.log('Open Mail');
+  let a = document.createElement('a')
+  a.href = `mailto:${emailReceiver}?subject=${subject}&body=Hello, my name ${name},My phone Number ${phone}, ${subject}, ${message}`
+     
+  a.click()
 
-    const a = document.createElement('a')
-    let emailReceiver = 'harihandika2441998@gmail.com'
-    a.href = `mailto:${emailReceiver}?subject=${subject}&body=Hallo my name ${name}, ${subject}, ${message}, my phone number ${phone}`
-    a.click();
+  // kata-kunci nama = {
+  //     proprty-name : value
+  // }
 
-    // Object
+  let dataObject = {
+      name: name,
+      email: email,
+      phone:phone,
+      subject:subject,
+      message: message,
+  }
 
-    const dataObject = {
-        name,
-        email,
-        phone,
-        subject,
-        message
-    }
-
-    console.log(dataObject);
+  console.table(dataObject)
 }
